73b35d6b4008cbe6349b2b1860616891d28ba47c
yutaro_matsumoto <spurs03170629@gmail.com>
2025-01-30 23:20:13 +0900

    認証の基盤作り

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,40 @@
 <script lang="ts">
 	import '../app.css'
 	import Layout from '$ui/_layout/Layout.svelte'
+
+	/** start supabase */
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+	import type { LayoutData } from './$types'
+	// import { setUser } from '$globalStates/user'
+	// import { setSupabase } from '$globalStates/supabase'
+	// import { setSession } from '$globalStates/session'
+
+	// // ここのdataは、おそらく+layout.tsのload関数の返り値
+	let { data, children } = $props()
+  let { session, supabase } = $derived(data)
+
+
+	// $: ({ supabase, session, userData } = data)
+
+	// loadしたものは+layout内でcontextに入れた方が良いと判断
+	// $: setUser(userData)
+	// $: setSupabase(supabase)
+	// $: setSession(session)
+
+	// 以下の処理の必要性について理解できてない。auth-helper利用時とそこまで変更はなさそうだが。
+	onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth')
+      }
+    })
+
+    return () => data.subscription.unsubscribe()
+  })
+	/** end supabase */
 </script>
 
 <Layout>
-	<slot />
+	{@render children()}
 </Layout>
