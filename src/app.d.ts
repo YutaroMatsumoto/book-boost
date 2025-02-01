656a643469a761e1e42d5ecd2ee638a33a12326e
yutaro_matsumoto <spurs03170629@gmail.com>
2025-02-01 23:16:38 +0900

    app.d.tsの型修正

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,13 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+import type { Session, SupabaseClient, User } from "@supabase/supabase-js";
+import type { Database } from "./database";
+
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
     interface Locals {
-      supabase: SupabaseClient;
+      supabase: SupabaseClient<Database>;
       safeGetSession: () => Promise<{
         session: Session | null;
         user: User | null;
