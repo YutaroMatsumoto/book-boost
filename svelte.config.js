bcf8aebf18d5204825e40d088018bb916b167cdb
yutaro_matsumoto <spurs03170629@gmail.com>
2025-02-02 18:31:59 +0900

    svelte.configの修正

--- svelte.config.js
+++ svelte.config.js
@@ -16,6 +16,9 @@ const config = {
       "$model/*": "src/components/model/*",
       "$ui/*": "src/components/ui/*",
       "$models/*": "src/models/*",
+      "$database/*": "src/database/*",
+      "$globalStates/*": "src/globalStates/*",
+      "$repositories/*": "src/repositories/*",
     },
     files: {
       hooks: {
