33caec2a009af8cb46ea6a62f4a90e5b1bd102df
yutaro_matsumoto <spurs03170629@gmail.com>
2025-02-02 22:04:36 +0900

    ログインボタンの実装

---
+++ src/components/ui/_button/LoginButton/LoginButton.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { getSupabase } from '$globalStates/supabase.svelte'
+	import GoogleIcon from '$ui/_icon/GoogleIcon.svelte'
+  import { handleSignIn } from '$repositories/auth/repository';
+
+	/**
+	 * styleはgoogleのガイドラインに従う
+	 * https://developers.google.com/identity/branding-guidelines?hl=ja
+	 */
+
+	const supabase = getSupabase()
+</script>
+
+<button class="flex items-center border border-gray-300 rounded-sm shadow-md p-2 cursor-pointer hover:bg-green-50" onclick={() => handleSignIn($supabase)}>
+	<GoogleIcon />
+	<span class="ml-6">Googleでログイン</span>
+</button>
